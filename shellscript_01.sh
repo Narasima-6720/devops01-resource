96e107f5750be2cf75ea623202c67cfcec0e50cf
Ubuntu <ubuntu@ip-172-31-34-114.us-east-2.compute.internal>
2024-02-13 18:38:42 +0000

    ok

---
+++ shellscript_01.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+set -e
+set -o
+#printing the total processes running 
+ 
+nproc
+
+
+#totalmemory
+
+free
+
+
+#disk space
+
+df
+
+
+#list of files and folders
+
+ls -ltr
+
+
+
